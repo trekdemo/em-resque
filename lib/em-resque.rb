f966d6abf69388cfd87872d15585b344a8aae9e4
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2011-12-23 13:37:27 +0100

    Different naming for em-versions to be able to include the original code

--- lib/em-resque.rb
+++ lib/em-resque.rb
@@ -1,29 +1,26 @@
 require 'resque'
 require 'em-synchrony/em-redis'
 
-module EventMachine
-  include Resque
-
-  module Resque
-    def redis=(server)
-      case server
-      when String
-        if server =~ /redis\:\/\//
-          redis = EM::Protocols::Redis.connect(:url => server, :thread_safe => true)
-        else
-          server, namespace = server.split('/', 2)
-          host, port, db = server.split(':')
-          redis = EM::Protocols::Redis.new(:host => host, :port => port,
-                                           :thread_safe => true, :db => db)
-        end
-        namespace ||= :resque
-
-        @redis = Redis::Namespace.new(namespace, :redis => redis)
-      when Redis::Namespace
-        @redis = server
+module EM::Resque
+  extend Resque
+  def redis=(server)
+    case server
+    when String
+      if server =~ /redis\:\/\//
+        redis = EM::Protocols::Redis.connect(:url => server, :thread_safe => true)
       else
-        @redis = Redis::Namespace.new(:resque, :redis => server)
+        server, namespace = server.split('/', 2)
+        host, port, db = server.split(':')
+        redis = EM::Protocols::Redis.new(:host => host, :port => port,
+                                         :thread_safe => true, :db => db)
       end
+      namespace ||= :resque
+
+      @redis = Redis::Namespace.new(namespace, :redis => redis)
+    when Redis::Namespace
+      @redis = server
+    else
+      @redis = Redis::Namespace.new(:resque, :redis => server)
     end
   end
 end
