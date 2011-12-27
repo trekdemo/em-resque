45fcb8ae5daf2bc8542743930f3dad76b1eb504a
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2011-12-27 13:28:41 +0100

    Proper handling of accumulator

--- lib/em-resque/tasks.rb
+++ lib/em-resque/tasks.rb
@@ -13,9 +13,9 @@ namespace :em_resque do
     bool_keys = %w(LOGGING VERBOSE VVERBOSE)
 
     opts = ENV.reduce({}) do |acc, (k, v)|
-      acc.merge(k.downcase.to_sym => v.to_i) if integer_keys.any?{|ik| ik == k}
-      acc.merge(k.downcase.to_sym => v.to_s) if string_keys.any?{|sk| sk == k}
-      acc.merge(k.downcase.to_sym => v == '1' || v.downcase == 'true') if bool_keys.any?{|bk| bk == k}
+      acc = acc.merge(k.downcase.to_sym => v.to_i) if integer_keys.any?{|ik| ik == k}
+      acc = acc.merge(k.downcase.to_sym => v.to_s) if string_keys.any?{|sk| sk == k}
+      acc = acc.merge(k.downcase.to_sym => v == '1' || v.downcase == 'true') if bool_keys.any?{|bk| bk == k}
       acc
     end
 
