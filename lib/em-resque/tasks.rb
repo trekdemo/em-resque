a23d545befa3315686b1408e7a3a8182ccfbe761
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2011-12-27 15:21:10 +0100

    Remove rails support for now

--- lib/em-resque/tasks.rb
+++ lib/em-resque/tasks.rb
@@ -6,21 +6,9 @@ namespace :em_resque do
   task :setup
 
   desc "Start an async Resque worker"
-  task :work => [ :preload, :setup ] do
+  task :work => [ :setup ] do
     require 'em-resque'
 
     EM::Resque::WorkerMachine.new(TaskHelper.parse_opts_from_env).start
-  end
-
-  # Preload app files if this is Rails
-  task :preload => :setup do
-    if defined?(Rails) && Rails.respond_to?(:application)
-      # Rails 3
-      Rails.application.eager_load!
-    elsif defined?(Rails::Initializer)
-      # Rails 2.3
-      $rails_rake_task = false
-      Rails::Initializer.run :load_application_classes
-    end
   end
 end
