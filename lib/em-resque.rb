9bef0902ae90d42b3c3d187dfe292fa457180739
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2012-08-08 09:26:12 -0400

    Allowing hashes as arguments for Redis server.

--- lib/em-resque.rb
+++ lib/em-resque.rb
@@ -9,30 +9,36 @@ module EM::Resque
 
   def self.initialize_redis(server, pool_size = 1)
     case server
-    when String
-      opts = if server =~ /redis\:\/\//
-               uri = URI.parse(server)
-               {:host => uri.host, :port => uri.port}
-             else
-               server, namespace = server.split('/', 2)
-               host, port, db = server.split(':')
-               {:host => host, :port => port, :thread_safe => true, :db => db}
-             end
+      when String
+        opts = if server =~ /redis\:\/\//
+                 uri = URI.parse(server)
+                 {:host => uri.host, :port => uri.port}
+               else
+                 server, namespace = server.split('/', 2)
+                 host, port, db = server.split(':')
+                 {:host => host, :port => port, :thread_safe => true, :db => db}
+               end
 
-      namespace ||= :resque
+        redis = EventMachine::Synchrony::ConnectionPool.new(:size => pool_size) do
+          EM::Protocols::Redis.connect(opts)
+        end
+      when Redis::Namespace
+        Resque.redis = server
+      when Hash
+        if server[:namespace]
+          namespace = server[:namespace]
+          server.delete(:namespace)
+        end
 
-      redis = EventMachine::Synchrony::ConnectionPool.new(:size => pool_size) do
-        EM::Protocols::Redis.connect(opts)
-      end
-
-      Resque.redis = Redis::Namespace.new(namespace, :redis => redis)
-    when Redis::Namespace
-      Resque.redis = server
-    else
-      redis = EventMachine::Synchrony::ConnectionPool.new(:size => pool_size) do
-        server
-      end
-      Resque.redis = Redis::Namespace.new(namespace, :redis => redis)
+        redis = EventMachine::Synchrony::ConnectionPool.new(:size => pool_size) do
+          EM::Protocols::Redis.connect(server)
+        end
+      else
+        redis = EventMachine::Synchrony::ConnectionPool.new(:size => pool_size) do
+          server
+        end
     end
+    namespace ||= :resque
+    Resque.redis = Redis::Namespace.new(namespace, :redis => redis)
   end
 end
